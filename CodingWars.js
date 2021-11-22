25b7480f0a23ed91f059785bdeeada80960fba59
Sky Lin <linsky519@gmail.com>
2021-11-22 16:06:48 -0700

    4-6 completed

--- CodingWars.js
+++ CodingWars.js
@@ -10,7 +10,7 @@ const tacos = [taco, taco1, taco2]
 
 
 
-// write a function that takes an tacoOBJ and a id and returns a new object with name changed to "changed"
+// 1) write a function that takes an tacoOBJ and a id and returns a new object with name changed to "changed"
 const updateTacoName = (name, id) => {
   let new_arry = tacos.map((taco) => {
     if (taco.id !== id) {
@@ -25,7 +25,7 @@ const updateName = updateTacoName("Changed", 1);
 console.log("Updated Name: ", updateName);
 
 
-// write a function that takes an tacoOBJ and return some formatted html (will be a string technically)
+// 2) write a function that takes an tacoOBJ and return some formatted html (will be a string technically)
 const formattedTaco = () => {
   return tacos.map((taco) => {
     return `<p>${taco.name} taco is ${taco.about}</p>`;
@@ -36,7 +36,7 @@ const html = formattedTaco();
 console.log(html);
 
 
-// write a function that takes an tacoOBJ and return formatted price with .00 
+// 3) write a function that takes an tacoOBJ and return formatted price with .00 
 const formattedPrice = (taco) => {
   let newPrice = taco.price.toFixed(2);
   return {...taco, price: newPrice}
@@ -47,26 +47,41 @@ console.log(formattedPrice(taco1));
 console.log(formattedPrice(taco2));
 
 
-// create a new array where all of the prices is formatted with .00
-// use .map (taco)
+// 4) create a new array where all of the prices is formatted with .00 hint use .map (taco)
+const tacoPrice = () => {
+  return tacos.map((taco) => {
+    let tacoArr = taco.price.toFixed(2);
+    return { ...taco, price: tacoArr };
+  });
+};
+
+console.log(tacoPrice());
+
+
+// 5) write a function that takes an array and logs each item in the array hint forEach
+const printTaco = () => {
+  let printInfo = tacos.forEach((taco) => {
+    return console.log(`${taco.id}) ${taco.name} taco, $${taco.price}, It's ${taco.about}`)
+  });
+  return printInfo
+}
+
+printTaco(tacos)
+
+
+// 6) use the find method to return the object with id:1
+const findTaco = (arr, id) => {
+  return arr.find((object) =>object.id === id );
+};
+
+console.log(findTaco(tacos, 1))
+
+
+// 7) return a new array with all prices greater than 19 hint use forEach
 
 
 
-// write a function that takes an array and logs each item in the array hint forEach
-
-
-
-// use the find method to return the object with id:1
-
-
-
-// return a new array with all prices greater than 19
-
-
-
-// return a new array with a 'about' key where it is a combo of
-
-// name price and about
+// 8) return a new array with a 'about' key where it is a combo of name price and about
 
 
 
